78a9faa49c704886f6bb8e4a82eae718e9d98a02
Caleig <1359939188@qq.com>
2026-08-02 23:26:29 +0800

    hjson

--- obj/Debug/net8.0/ThoriumAccessoryExpansion.AssemblyInfo.cs
+++ obj/Debug/net8.0/ThoriumAccessoryExpansion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThoriumAccessoryExpansion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef81e2817cde45510e24c4905d6dfcac2bcb978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea8eaa535d699373aa9bf2f363040c4becf9ad3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThoriumAccessoryExpansion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThoriumAccessoryExpansion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
